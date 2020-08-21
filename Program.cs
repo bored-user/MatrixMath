90650e3760e98505fac87efb35ca973b46a70153
bored-user <joao.guimaraes.alves.vaz@gmail.com>
2020-08-20 21:33:52 -0300

    Remove LINQ assembly reference & full refactor

--- Program.cs
+++ Program.cs
@@ -1,44 +1,82 @@
 ﻿using System;
 using System.Text.RegularExpressions;
-using System.Linq;
 
 namespace MatrixMath
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             try
             {
-                string matrix;
-                long rows;
-
-                Console.Write("Input matrix using a comma as separator (e.g 1,2,3,4,5)\n> Your choice: ");
-                matrix = Console.ReadLine();
-                Console.Write("> Input number of rows of that matrix: ");
-                rows = Convert.ToInt64(Console.ReadLine());
-
-                if (new Regex(@"\d+(,\d+)*(\.\d*)?").Matches(matrix).Count < 1)
-                    throw new FormatException($"Expecting matrix to be in 'N1,N2,N3,N4,Nn' format. Got '{matrix}'");
-
-                if (rows < 1)
-                    throw new IndexOutOfRangeException("Matrix cannot have 0 or less rows");
-
-                string[] matrixArray = matrix.Split(',');
-                object[] isValid = Matrix.IsValid(matrixArray, rows);
-                if(!(bool)isValid[0])
-                    throw new FormatException((string)isValid[1]);
-
-                Console.WriteLine(new Matrix(Matrix.Convert(matrixArray), rows));
+                Console.Write("Howdy!\nWhat mathematical operation would you like to perform?\n\n1). Matrix addition\n2). Matrix subtraction\n3). Matrix multiplication\n4). Matrix division\n5). Matrix determinante\n\n> Your choice: ");
+                Console.WriteLine(Program.Caller(Console.ReadLine()));
             }
             catch (Exception exc)
             {
-                Console.Write("Either you did something wrong or I did :(\n\nPress 'd' to read the excpetion or anything else to exit: ");
-                if (Console.ReadLine() == "d")
-                    Console.WriteLine($"{exc.GetType()}: {exc.Message}");
-
-                Environment.Exit(-1);
+                Console.WriteLine($"ERROR! - {exc.GetType()}: {exc.Message}");
             }
+        }
+
+        private static Matrix Caller(string input)
+        {
+            Matrix matrix1;
+            Matrix matrix2;
+
+            switch (input)
+            {
+                case "1":
+                    matrix1 = Program.GetMatrix(1);
+                    matrix2 = Program.GetMatrix(2);
+
+                    return Matrix.Sum(matrix1, matrix2);
+
+
+                case "2":
+                    matrix1 = Program.GetMatrix(1);
+                    matrix2 = Program.GetMatrix(2);
+
+                    return Matrix.Subtract(matrix1, matrix2);
+
+
+                case "3":
+                    matrix1 = Program.GetMatrix(1);
+                    matrix2 = Program.GetMatrix(2);
+
+                    return Matrix.Multiply(matrix1, matrix2);
+
+
+                default:
+                    return new Matrix();
+            }
+        }
+
+        public static Matrix GetMatrix(int num = 0)
+        {
+            string matrix;
+            long rows;
+            Regex regex = new Regex(@"\d+(,\d+)*(\.\d*)?");
+            string[] matrixArray;
+            object[] isValid;
+
+            Console.Write($"Input matrix{(num > 0 ? $" {num}" : "")} using a comma as separator (e.g 1,2,3,4,5,6)\n> Your choice: ");
+            matrix = Console.ReadLine();
+            Console.Write($"> Input number of rows of matrix{(num > 0 ? $" {num}" : "")}: ");
+            rows = Convert.ToInt64(Console.ReadLine());
+
+            if (regex.Matches(matrix).Count < 1)
+                throw new FormatException($"Expecting matrix to be in 'N1,N2,N3,N4,Nn' format. Got '{matrix}'");
+
+            if (rows < 1)
+                throw new IndexOutOfRangeException("Matrix cannot have 0 or less rows");
+
+            matrixArray = matrix.Split(',');
+            isValid = Matrix.IsValid(matrixArray, rows);
+
+            if (!(bool)isValid[0])
+                throw new FormatException((string)isValid[1]);
+
+            return new Matrix(Matrix.Convert(matrixArray), rows);
         }
     }
 }
