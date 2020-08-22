bfd593957638e2904a7889c057372982da95d18a
bored-user <joao.guimaraes.alves.vaz@gmail.com>
2020-08-22 14:23:22 -0300

    Implemented multiplication, general improvements

--- Matrix.cs
+++ Matrix.cs
@@ -6,7 +6,7 @@ namespace MatrixMath
 {
     public class Matrix
     {
-        public List<long[]> matrix = new List<long[]>();
+        public long[,] matrix;
         public long rows = 0;
         public long cols = 0;
 
@@ -19,27 +19,24 @@ namespace MatrixMath
         public Matrix(long[] matrix, long rows)
         {
             this.style["biggestLength"] = matrix.OrderBy(n => n.ToString().Length).ToArray().Last().ToString().Length;
-
             this.cols = matrix.Length / rows;
             this.rows = rows;
+            this.matrix = new long[this.rows, this.cols];
 
-            long[] row = new long[this.cols];
-            long count = 0;
+            long row = 0;
+            long col = 0;
 
             foreach (long n in matrix)
             {
-                if (count == this.cols)
+                if (col == this.cols)
                 {
-                    this.matrix.Add((long[])row.Clone());
-                    Array.Clear(row, 0, row.Length);
-                    count = 0;
+                    col = 0;
+                    row++;
                 }
 
-                row[count] = n;
-                count++;
+                this.matrix[row, col] = n;
+                col++;
             }
-
-            this.matrix.Add(row);
         }
 
         public Matrix()
@@ -51,17 +48,15 @@ namespace MatrixMath
         private static Matrix SumSubWrapper(Matrix matrix1, Matrix matrix2, bool sum)
         {
             Matrix.IsValid(matrix1, matrix2, sum ? "sum" : "sub");
-
             string res = "";
-            long[] row = new long[matrix1.cols];
 
-            for (int i = 0; i < matrix1.rows; i++) // each row
+            for (int row = 0; row < matrix1.rows; row++)
             {
-                for (int j = 0; j < matrix1.matrix[i].Count(); j++) // each item in row
+                for (int col = 0; col < matrix1.cols; col++)
                 {
                     res += sum ?
-                        $"{matrix1.matrix[i][j] + matrix2.matrix[i][j]}," :
-                        $"{matrix1.matrix[i][j] - matrix2.matrix[i][j]},";
+                        $"{matrix1.matrix[row, col] + matrix2.matrix[row, col]}," :
+                        $"{matrix1.matrix[row, col] - matrix2.matrix[row, col]},";
                 }
             }
 
@@ -81,29 +76,43 @@ namespace MatrixMath
         public static Matrix Multiply(Matrix matrix1, Matrix matrix2)
         {
             Matrix.IsValid(matrix1, matrix2, "mult");
-            long[] res;
+            string res = "";
 
-            
+            for (long row = 0; row < matrix1.rows; row++)
+            {
+                for (long col = 0; col < matrix2.cols; col++)
+                {
+                    long _res = 0;
+                    for (long i = 0; i < matrix1.cols; i++)
+                        _res += matrix1.matrix[row, i] * matrix2.matrix[i, col];
 
-            return new Matrix();
+                    res += $"{_res},";
+                }
+            }
+
+            return new Matrix(Matrix.Convert(res.Substring(0, res.Length - 1).Split(',')), matrix1.rows);
         }
 
         public static void IsValid(Matrix matrix1, Matrix matrix2, string operation)
         {
-            if (operation == "sum" || operation == "sub")
-            {
-                if (matrix1.cols != matrix2.cols || matrix1.rows != matrix2.rows)
-                    throw new FormatException("Both matrices must have the same number of rows and cols.");
-            }
-            else if(operation == "mult" || operation == "div")
-            {
-                if (matrix1.rows != matrix2.cols || matrix1.cols != matrix2.rows)
-                    throw new FormatException("Matrix 1 cols must be equal to Matrix 2 rows and vice versa.");
-            }
 
+            switch (operation)
+            {
+                case "sum":
+                case "sub":
+                    if (matrix1.cols != matrix2.cols || matrix1.rows != matrix2.rows)
+                        throw new FormatException("Both matrices must have the same number of rows and cols.");
+                    break;
+
+                case "mult":
+                case "div":
+                    if (matrix1.rows != matrix2.cols || matrix1.cols != matrix2.rows)
+                        throw new FormatException("Matrix 1 cols must be equal to Matrix 2 rows and vice versa.");
+                    break;
+            }
         }
 
-        // Matrix will only be valid if its length isn't a prime number
+        // // Matrix will only be valid if its length isn't a prime number
         public static object[] IsValid(string[] matrix, long rows)
         {
             if (matrix.Length % rows != 0) return new object[] { false, "Matrix length and row number aren't multiples" };
@@ -133,16 +142,19 @@ namespace MatrixMath
         {
             string res = "";
             string padding = "".PadLeft(this.style["identation"], ' ');
+            long n;
 
-            foreach (long[] row in this.matrix)
+            for (long row = 0; row < this.rows; row++)
             {
                 res += "|";
 
-                foreach (long n in row)
-                    res += n > 0 ?
+                for (long col = 0; col < this.cols; col++)
+                {
+                    n = this.matrix[row, col];
+                    res += Math.Abs(n) == n ?
                         $"{padding}{n.ToString().PadRight(this.style["biggestLength"], ' ')}{padding}" :
                         $"{padding}-{Math.Abs(n).ToString().PadRight(this.style["biggestLength"] - 1, ' ')}{padding}";
-
+                }
 
                 res += "|\n";
             }
