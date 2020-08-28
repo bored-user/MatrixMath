95ddd2fe271e223973d12da9822126242e6b7253
bored-user <joao.guimaraes.alves.vaz@gmail.com>
2020-08-28 20:38:30 -0300

    Can now calculate 3x3 matrices

--- Matrix.cs
+++ Matrix.cs
@@ -146,6 +146,11 @@ namespace MatrixMath
 
                 case 2:
                     return this.matrix[0, 0] * this.matrix[1, 1] - this.matrix[0, 1] * this.matrix[1, 0];
+
+                case 3:
+                    // Using Sarrus
+                    return (this.matrix[0, 0] * this.matrix[1, 1] * this.matrix[2,2] + this.matrix[0, 1] * this.matrix[1, 2] * this.matrix[2, 0] + this.matrix[0, 2] * this.matrix[1, 0] * this.matrix[2, 1]) -
+                           (this.matrix[0, 1] * this.matrix[1, 0] * this.matrix[2,2] + this.matrix[0, 0] *this.matrix[1, 2] * this.matrix[2, 1] + this.matrix[0, 2] * this.matrix[1, 1] * this.matrix[2, 0]);
             }
 
             return 0;
